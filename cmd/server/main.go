8e6db4d4eaf00e6e3c6f368b140e4685d323455f
Quạ béo phì <root@lulu>
2018-07-24 06:41:01 -0400

    CRUD For Category
    - Complate API for Category

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/LuanNgoMinh/example-go/endpoints"
 	serviceHttp "github.com/LuanNgoMinh/example-go/http"
 	"github.com/LuanNgoMinh/example-go/service"
+	categorySvc "github.com/LuanNgoMinh/example-go/service/category"
 	userSvc "github.com/LuanNgoMinh/example-go/service/user"
 )
 
@@ -57,6 +58,10 @@ func main() {
 				userSvc.NewPGService(pgDB),
 				userSvc.ValidationMiddleware(),
 			).(userSvc.Service),
+
+			CategoryService: service.Compose(
+				categorySvc.NewPGService(pgDB),
+			).(categorySvc.Service),
 		}
 	)
 	defer closeDB()
---
+++ domain/category.go
@@ -0,0 +1,7 @@
+package domain
+
+// Category describe Categories in systenm
+type Category struct {
+	Model
+	Name string `json:"name"`
+}
